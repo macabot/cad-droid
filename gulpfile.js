2bbe30fed9bb5058c96ff6e6983c7645fce9c571
Michael Cabot <michael.a.cabot@gmail.com>
2016-01-25 20:38:25 +0100

    log command. remove old code. remove unused dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 'use strict';
 
+// imports
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
     exec = require('child_process').exec,
     through = require('through2'),
     path = require('path'),
     gutil = require('gulp-util'),
-    mkdirp = require('mkdirp');
+    mkdirp = require('mkdirp'),
+    colors = require('colors');
 
+// constants
 var source = 'src/**/*.escad',
     target = 'build',
     extopenscadPath = '~/.cabal/bin/extopenscad'; // TODO should be extopenscad
@@ -18,14 +21,15 @@ var extopenscad = function extopenscad(options, sync) {
         var fileRelative = file.relative;
         var fileDirname = path.dirname(fileRelative);
         var fileName = path.basename(filePath);
-        
+        // create output path
         var outName = gutil.replaceExtension(fileName, '.stl');
         var outDirname = path.join(target, fileDirname);
         mkdirp(outDirname, function() {
             var outPath = path.join(outDirname, outName);
-        
-//            console.log(filePath, fileRelative, fileDirname, fileName, outPath);
-            exec(extopenscadPath + ' -o ' + outPath + ' ' + filePath, function(err, stdout, stderr) {
+            var command = extopenscadPath + ' -o ' + outPath + ' ' + filePath;
+            // execute extopenscad
+            console.log(command.green);
+            exec(command, function(err, stdout, stderr) {
                 console.log(stdout);
                 console.log(stderr);
                 cb(null, file);
@@ -39,36 +43,4 @@ gulp.task('default', function() {
         .pipe(watch(source))
         .pipe(extopenscad());
 });
-
-/*
-gulp.task('default', ['watch']);
-
-gulp.task('extopenscad', function(cb) {
-    console.log(arguments);
-    exec('echo "hello world"', function(err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
-    });
-});
-
-//gulp.watch(source, ['extopenscad']);
-
-function extopenscad(cb) {
-    console.log(arguments);
-    console.log(cb);
-}
-
-gulp.task('watch', function() {
-    gulp.watch(source, ['extopenscad']);
-});
-
-
-gulp.task('default', function() {
-    return gulp.src(source)
-        .pipe(watch(source))
-        .pipe(extopenscad);
-});
-
-*/
 
